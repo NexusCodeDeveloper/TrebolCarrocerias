bffede70ad5be8859c65884114ed8f7bfa23c74f
Pablo Basso <pablobasso25@gmail.com>
2026-06-23 17:57:19 -0300

    refactor: Actualizar el fondo del componente glass a color sólido

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Productos from './components/Productos'
-import Galeria from './components/Galeria'
-import Tecnologia from './components/Tecnologia'
-import Testimonios from './components/Testimonios'
-import Contacto from './components/Contacto'
-import Footer from './components/Footer'
-import WhatsAppButton from './components/WhatsAppButton'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Productos from "./components/Productos";
+import Galeria from "./components/Galeria";
+/* import Tecnologia from './components/Tecnologia' */
+import Testimonios from "./components/Testimonios";
+import Contacto from "./components/Contacto";
+import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
 
 export default function App() {
   return (
@@ -15,11 +15,11 @@ export default function App() {
       <Hero />
       <Productos />
       <Galeria />
-      <Tecnologia />
+      {/*  <Tecnologia /> */}
       <Testimonios />
       <Contacto />
       <Footer />
       <WhatsAppButton />
     </div>
-  )
+  );
 }
